c0c059826e2f1588a49cc6900218af6bfc79ed62
Dave Morse <morsed@wwt.com>
2024-03-28 15:46:32 -0500

    npm install @reduxjs/toolkit react-redux @types/react-redux

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
 import { SearchScreen } from "./components/SearchScreen";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <SearchScreen />
-    </main>
+    <Provider store={store}>
+      <main className={styles.main}>
+        <SearchScreen />
+      </main>
+    </Provider>
   );
 }
